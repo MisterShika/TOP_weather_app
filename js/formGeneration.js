2088c1d52b4a7068c135d522ff626a77d4a95715
Kyle W <125979751+MisterShika@users.noreply.github.com>
2024-10-16 20:49:37 +0900

    Project complete for now. Will revisit later in react

--- js/formGeneration.js
+++ js/formGeneration.js
@@ -1,3 +1,43 @@
-export function formGeneration () {
-    
+
+export function formGeneration (createdElement) {
+    return new Promise((resolve) => {
+        const theForm = document.createElement('form');
+            theForm.classList.add('mainForm');
+        const firstLocationLabel = document.createElement('label');
+            firstLocationLabel.htmlFor = 'location1';
+            firstLocationLabel.innerText = 'Enter First Location: ';
+        const firstLocation = document.createElement('input');
+            firstLocation.type = 'text';
+            firstLocation.name = 'location1';
+            firstLocation.id = 'location1';
+            firstLocation.placeholder = 'e.g. London, Tokyo, etc...';
+        const secondLocationLabel = document.createElement('label');
+            secondLocationLabel.htmlFor = 'location2';
+            secondLocationLabel.innerText = 'Enter Second Location: ';
+        const secondLocation = document.createElement('input');
+            secondLocation.type = 'text';
+            secondLocation.name = 'location2';
+            secondLocation.id = 'location2';
+            secondLocation.placeholder = 'e.g. London, Tokyo, etc...';
+        const submitButton = document.createElement('button');
+            submitButton.type = 'submit';
+            submitButton.innerText = 'Get Weather';
+
+        theForm.appendChild(firstLocationLabel);
+        theForm.appendChild(firstLocation);
+        theForm.appendChild(secondLocationLabel);
+        theForm.appendChild(secondLocation);
+        theForm.appendChild(submitButton);
+
+        theForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const formData = new FormData(theForm);
+            createdElement.innerHTML = '';
+            console.log(Object.fromEntries(formData.entries()));
+            resolve(Object.fromEntries(formData.entries()));
+            theForm.reset();
+        });
+
+        createdElement.appendChild(theForm);
+    });
 }
--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 import WeatherItem from './weather.js';
 import {initDisplay, updateDisplay} from './guiFunctions.js';
+import { formGeneration } from './formGeneration.js';
 
+const mainArea = document.getElementById('forecastBlock');
 
-(async () => {
-    const itemOne = new WeatherItem('sapporo');
-    initDisplay(itemOne, 'test1');
+function startWeatherBox (location, boxID) {
+    (async () => {
+        const theWeather = new WeatherItem(location);
+        initDisplay(theWeather, boxID);
+        
+        setInterval(() => {
+            console.log('Interval fired');
+            updateDisplay(theWeather, boxID);
+        }, 300000);
+    })();
+}
+
+formGeneration(mainArea).then((data) => {
+    let formData = data;
+    let boxID = 0;
+    Object.values(formData).forEach((value) => {
+        console.log(value);
+        startWeatherBox(value, `'box-${boxID}'`)
+        boxID++;
+    });
+});
+
+// (async () => {
+//     const itemOne = new WeatherItem('sapporo');
+//     initDisplay(itemOne, 'test1');
     
-    setInterval(() => {
-        console.log('Interval fired');
-        updateDisplay(itemOne, 'test1');
-    }, 300000);
-})();
+//     setInterval(() => {
+//         console.log('Interval fired');
+//         updateDisplay(itemOne, 'test1');
+//     }, 300000);
+// })();
 
-(async () => {
-    const itemTwo = new WeatherItem('venice FL');
-    initDisplay(itemTwo, 'test2');
+// (async () => {
+//     const itemTwo = new WeatherItem('venice FL');
+//     initDisplay(itemTwo, 'test2');
     
-    setInterval(() => {
-        console.log('Interval fired');
-        updateDisplay(itemTwo, 'test2');
-    }, 300000);
-})();
+//     setInterval(() => {
+//         console.log('Interval fired');
+//         updateDisplay(itemTwo, 'test2');
+//     }, 300000);
+// })();
